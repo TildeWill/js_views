8c6bd6fa65d915ab809d5e74405f1dc958bba5df
Colin Deeb and Will Read <pair+colin+will@pivotallabs.com>
2013-08-18 21:47:02 -0700

    Show how you'd have to assemble your own HTML in the JS

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('#dog-form').delegate("form", "ajax:success", function(evt, data, status, xhr) {
-    $('#dog-form').empty();
     $('#dog-show').html(xhr.responseText);
+    $('#dog-form').empty();
   })
 
   $('#new-dog-link').bind("ajax:success", function(evt, data, status, xhr) {
-    $('#dog-form').append(xhr.responseText);
+    $('#dog-form').html(xhr.responseText);
   })
 
   $('#dog-list').delegate(".edit-link", "ajax:success", function(evt, data, status, xhr) {
